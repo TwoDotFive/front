2ff54ab861f1e29270e509bed08d32776a35b2be
westofsky <westofsky159@gmail.com>
2024-08-18 11:26:46 +0900

    setting: svg 및 png 추가

---
+++ public/icons/index.ts
@@ -0,0 +1,39 @@
+import IconCalender from './common/icon_calender.svg';
+import IconCancel from './common/icon_cancel.svg';
+import IconLeftArrow from './common/icon_left_arrow.svg';
+import IconMoreMenu from './common/icon_more_menu.svg';
+import IconPencil from './common/icon_pencil.svg';
+import IconPlus from './common/icon_plus.svg';
+import IconUpload from './common/icon_upload.svg';
+
+import IconDefaultPin from './map/icon_default_pin.svg';
+import IconNowPin from './map/icon_now_pin.svg';
+
+import IconHome from './navigation/icon_home.svg';
+import IconHomeActive from './navigation/icon_home_active.svg';
+import IconMap from './navigation/icon_map.svg';
+import IconMapActive from './navigation/icon_map_active.svg';
+import IconPeople from './navigation/icon_people.svg';
+import IconPeopleActive from './navigation/icon_people_active.svg';
+import IconSearch from './navigation/icon_search.svg';
+import IconSearchActive from './navigation/icon_search_active.svg';
+
+export {
+	IconCalender,
+	IconCancel,
+	IconLeftArrow,
+	IconMoreMenu,
+	IconPencil,
+	IconPlus,
+	IconUpload,
+	IconDefaultPin,
+	IconNowPin,
+	IconHome,
+	IconHomeActive,
+	IconMap,
+	IconMapActive,
+	IconPeople,
+	IconPeopleActive,
+	IconSearch,
+	IconSearchActive,
+};

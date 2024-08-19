cdc290780c6ebfcdf879bf457b92957e83e8370a
westofsky <westofsky159@gmail.com>
2024-08-19 17:45:49 +0900

    feat: locaiton 태그 추가

---
+++ src/components/tag/TagLocation.tsx
@@ -0,0 +1,11 @@
+import { Text } from '../common/Text';
+
+export default function TagLocation({ name }: { name: string }) {
+	return (
+		<div className="bg-warmOrange0 px-6pxr py-2pxr w-fit h-fit rounded-4pxr">
+			<Text fontSize={12} fontWeight={600} color="warmOrange500">
+				{name}
+			</Text>
+		</div>
+	);
+}

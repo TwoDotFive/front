2cd2f39aa6ac1cb5f0ad8a509e3e1683cea16311
westofsky <westofsky159@gmail.com>
2024-08-19 17:45:32 +0900

    feat: select button 컴포넌트 추가

---
+++ src/components/common/SelectButton.tsx
@@ -0,0 +1,19 @@
+import { Text } from './Text';
+
+type SelectButtonProps = {
+	isSelected: boolean;
+};
+
+export default function SelectButton({ isSelected }: SelectButtonProps) {
+	return (
+		<div
+			className={`w-fit h-fit rounded-44pxr px-10pxr py-4pxr hover:brightness-75 ${
+				isSelected ? 'bg-kboNavy text-gray000' : 'bg-gray50 text-gray600'
+			}`}
+		>
+			<Text fontSize={14} fontWeight={isSelected ? 700 : 400}>
+				{isSelected ? '취소' : '선택'}
+			</Text>
+		</div>
+	);
+}

e42911eb4ef059c6d08bd627d2edf8da13bd9239
westofsky <westofsky159@gmail.com>
2024-08-19 17:52:26 +0900

    feat: select 버튼 onClick 추가

--- src/components/button/SelectButton.tsx
+++ src/components/button/SelectButton.tsx
@@ -2,14 +2,19 @@ import { Text } from '../common/Text';
 
 type SelectButtonProps = {
 	isSelected: boolean;
+	onClick: () => void;
 };
 
-export default function SelectButton({ isSelected }: SelectButtonProps) {
+export default function SelectButton({
+	isSelected,
+	onClick,
+}: SelectButtonProps) {
 	return (
 		<div
 			className={`w-fit h-fit rounded-44pxr px-10pxr py-4pxr hover:brightness-75 ${
 				isSelected ? 'bg-kboNavy text-gray000' : 'bg-gray50 text-gray600'
 			}`}
+			onClick={onClick}
 		>
 			<Text fontSize={14} fontWeight={isSelected ? 700 : 400}>
 				{isSelected ? '취소' : '선택'}

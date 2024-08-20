0fd50ea2ebf1139cbf7992aa1c3282a83287ddc7
westofsky <westofsky159@gmail.com>
2024-08-20 16:02:13 +0900

    chore: 아이콘 폴더 구조 변경

---
+++ public/icons/common/index.ts
@@ -0,0 +1,23 @@
+import IconCalender from './icon_calender.svg';
+import IconCancel from './icon_cancel.svg';
+import IconLeftArrow from './icon_left_arrow.svg';
+import IconMoreMenu from './icon_more_menu.svg';
+import IconPencil from './icon_pencil.svg';
+import IconPlus from './icon_plus.svg';
+import IconUpload from './icon_upload.svg';
+import IconCheck from './icon_check.svg';
+import IconRadioDefault from './icon_radio_default.svg';
+import IconRadioSelected from './icon_radio_selected.svg';
+
+export {
+	IconCalender,
+	IconCancel,
+	IconLeftArrow,
+	IconMoreMenu,
+	IconPencil,
+	IconPlus,
+	IconUpload,
+	IconCheck,
+	IconRadioDefault,
+	IconRadioSelected,
+};
---
+++ public/icons/navigation/index.ts
@@ -0,0 +1,19 @@
+import IconHome from './icon_home.svg';
+import IconHomeActive from './icon_home_active.svg';
+import IconMap from './icon_map.svg';
+import IconMapActive from './icon_map_active.svg';
+import IconPeople from './icon_people.svg';
+import IconPeopleActive from './icon_people_active.svg';
+import IconSearch from './icon_search.svg';
+import IconSearchActive from './icon_search_active.svg';
+
+export {
+	IconHome,
+	IconHomeActive,
+	IconMap,
+	IconMapActive,
+	IconPeople,
+	IconPeopleActive,
+	IconSearch,
+	IconSearchActive,
+};

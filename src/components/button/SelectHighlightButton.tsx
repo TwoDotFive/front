7732fa5e535e263899ad5ada89196905f73aaa40
westofsky <westofsky159@gmail.com>
2024-08-20 16:28:07 +0900

    feat: Select Highlight 버튼 추가

---
+++ src/components/button/SelectHighlightButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { teams } from '@/constants/teams';
+import { Text } from '../common/Text';
+
+type SelectHighlightButtonProps = {
+	text: string;
+	isSelected?: boolean;
+	onClick?: () => void;
+};
+
+export default function SelectHighlightButton({
+	text,
+	isSelected = false,
+	onClick,
+}: SelectHighlightButtonProps) {
+	return (
+		<div
+			className={`w-full h-50pxr flex flex-col items-center justify-center rounded-8pxr px-16pxr py-11pxr gap-2pxr cursor-pointer ${
+				isSelected ? 'bg-kboBlue100 border border-kboBlue400' : 'bg-gray050'
+			}`}
+			onClick={onClick}
+		>
+			<Text
+				fontSize={16}
+				fontWeight={500}
+				color={isSelected ? 'kboBlue500' : 'gray700'}
+				className="whitespace-nowrap"
+			>
+				{text}
+			</Text>
+		</div>
+	);
+}

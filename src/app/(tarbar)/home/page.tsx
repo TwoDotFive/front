edb142762d85753ea4e539d278f74e0331df55d0
westofsky <westofsky159@gmail.com>
2024-08-24 05:23:08 +0900

    feat: 홈 화면 정보 보여주는 컴포넌트 추가

--- src/app/(tarbar)/home/page.tsx
+++ src/app/(tarbar)/home/page.tsx
@@ -1,9 +1,11 @@
+import InfoSection from '@/components/home/InfoSection';
 import TeamHeader from '@/components/home/TeamHeader';
 
 export default function page() {
 	return (
 		<main className="w-full h-full flex flex-col">
 			<TeamHeader teamCode="doosan" />
+			<InfoSection teamCode="doosan" />
 		</main>
 	);
 }
---
+++ src/components/home/LiveFanPool/LiveFanPool.tsx
@@ -0,0 +1,11 @@
+import { Text } from '../../common/Text';
+
+export default function LiveFanPool() {
+	return (
+		<section className="w-full flex flex-col">
+			<Text fontSize={20} fontWeight={700} color="gray800">
+				실시간 팬풀
+			</Text>
+		</section>
+	);
+}

0952752f2cac918977371f02da7ea95eaeec3cdd
westofsky <westofsky159@gmail.com>
2024-08-24 05:22:48 +0900

    feat: 팬풀 목록 보기 버튼 추가

---
+++ src/components/home/FanpoolButton.tsx
@@ -0,0 +1,15 @@
+import { Text } from '../common/Text';
+
+export default function FanpoolButton() {
+	/**
+	 * TODO count 계산하기
+	 */
+	const count = 136;
+	return (
+		<section className="w-full bg-primary text-center rounded-8pxr py-11pxr px-14pxr">
+			<Text fontSize={16} fontWeight={600} color="white">
+				현재 팬풀 {count}건
+			</Text>
+		</section>
+	);
+}

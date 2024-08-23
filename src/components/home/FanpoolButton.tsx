df96e377f579b957b735315f2f4be0f5b06cbadd
westofsky <westofsky159@gmail.com>
2024-08-24 06:12:30 +0900

    chore: cursor 추가

--- src/components/home/FanpoolButton.tsx
+++ src/components/home/FanpoolButton.tsx
@@ -6,7 +6,7 @@ export default function FanpoolButton() {
 	 */
 	const count = 136;
 	return (
-		<section className="w-full bg-primary text-center rounded-8pxr py-11pxr px-14pxr">
+		<section className="w-full bg-primary text-center rounded-8pxr py-11pxr px-14pxr cursor-pointer">
 			<Text fontSize={16} fontWeight={600} color="white">
 				현재 팬풀 {count}건
 			</Text>

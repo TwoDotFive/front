0046b790b7d29826bef3842026d4d3dc90aab8e9
westofsky <westofsky159@gmail.com>
2024-08-27 10:14:50 +0900

    feat: 팬풀 찾아보기 페이지 구성

--- src/app/(tarbar)/fanpool-find/page.tsx
+++ src/app/(tarbar)/fanpool-find/page.tsx
@@ -1,9 +1,12 @@
 import TapBar from '@/components/common/TapBar';
+import FindSection from '@/components/fanpool-find/FindSection';
 
 export default function page() {
 	return (
 		<section>
 			<TapBar text="팬풀 찾아보기" type="mid" isNextButton={false} />
+			<div className="h-24pxr" />
+			<FindSection />
 		</section>
 	);
 }
---
+++ src/components/fanpool-find/FindResult.tsx
@@ -0,0 +1,12 @@
+import FanpoolCard from '../common/fanpool/FanpoolCard';
+export default function FindResult() {
+	return (
+		<section className="flex flex-col gap-12pxr">
+			<FanpoolCard />
+			<FanpoolCard />
+			<FanpoolCard />
+			<FanpoolCard />
+			<FanpoolCard />
+		</section>
+	);
+}

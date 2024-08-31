6aad6ad5e188f0a1db045aba024d3d255cdfbea6
wonho1401 <wonho1401@naver.com>
2024-09-01 00:42:58 +0900

    feat: 가로 선 컴포넌트 추가

---
+++ src/components/common/InfinityLine.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface InfinityLineProps {
+    color?: string;
+    thickness?: string;
+    marginTop?: string;
+    marginBottom?: string;
+}
+
+export default function InfinityLine({color, thickness, marginTop, marginBottom}: InfinityLineProps) {
+  return (
+    <div className={`${color} ${thickness} w-full ${marginTop} ${marginBottom}`} />
+  )
+}

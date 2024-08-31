a35ae2a1e2d537855337c36b2f12f0080b3fd4b9
wonho1401 <wonho1401@naver.com>
2024-09-01 02:06:12 +0900

    feat: 카카오맵 API 추가

---
+++ src/components/fanpool-log/FanpoologDetail/useKakaoLoader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk'
+
+export default function useKakaoLoader() {
+  useKakaoLoaderOrigin({
+    appkey: process.env.NEXT_PUBLIC_KAKAO_APP_KEY as string,
+    libraries: ["clusterer", "services"]
+  })
+}

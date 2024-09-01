c0031008e6bcd1bb154458b5e84ecbb163c5b8e3
wonho1401 <wonho1401@naver.com>
2024-09-01 23:48:53 +0900

    feat: 팬풀로그 작성 버튼 핸들러 추가

--- src/app/(tarbar)/fanpool-log/page.tsx
+++ src/app/(tarbar)/fanpool-log/page.tsx
@@ -3,8 +3,15 @@ import Button from "@/components/common/Button";
 import HeaderBanner from "@/components/fanpool-log/HeaderBanner";
 import RecentFanpoolLog from "@/components/fanpool-log/RecentFanpoolLog";
 import TeamTravelog from "@/components/fanpool-log/TeamTravelog/TeamTravelog";
+import { useRouter } from "next/navigation";
 
 export default function page() {
+	const router = useRouter();
+
+	const handleFanpoologButton = () => {
+		router.push('/fanpool-log/create-log/step1');
+	}
+
 	return (
 		<div className="w-full flex flex-col">
 			<HeaderBanner imageUrl="/images/fanpool-log_ex.png" title="비오는날 경기 대신 서울나들이 ..." description="최대 두줄까지만이라고 하려고 했는데 여기 들어가는 문구가 뭔지 정해지지 않은 것 같아 " nickname="네임드호빵" userImgUrl="/images/kt.png" />
@@ -22,7 +29,7 @@ export default function page() {
 						enabledBackgroundColor={'bg-primary'}
 						disabledTextColor={'text-[#5679A3]'}
 						disabledBackgroundColor={'bg-primary'}
-						onClick={() => {}}
+						onClick={handleFanpoologButton}
 					/>
 				</div>	
 			</div>

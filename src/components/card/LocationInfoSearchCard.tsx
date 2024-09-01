f71da74c4a52a5434d5d6943fd92212caa5ce073
wonho1401 <wonho1401@naver.com>
2024-09-02 00:39:20 +0900

    style: Image border-radius 적용

--- src/components/card/LocationInfoSearchCard.tsx
+++ src/components/card/LocationInfoSearchCard.tsx
@@ -14,7 +14,7 @@ export default function LocationInfoSearchCard({image, name, location}: Location
         <div className='flex items-center gap-4pxr w-full h-50pxr'>
             <div className='flex items-center gap-8pxr flex-1'>
                 <div className='w-50pxr h-50pxr rounded-5pxr'>
-                    <img className='w-full h-full' src={image}/>
+                    <img className='w-full h-full rounded-5pxr' src={image}/>
                 </div>
                 <div className='flex flex-col justify-between items-start flex-1 self-stretch'>
                     <Text fontSize={16} fontWeight={700} color='gray700'>

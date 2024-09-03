abafe10a053b6d659155d7cabe2a6caa6fc939a8
wonho1401 <wonho1401@naver.com>
2024-09-03 23:50:24 +0900

    feat: BottomSheet 공통 컴포넌트 생성

---
+++ src/components/common/BottomSheet.tsx
@@ -0,0 +1,61 @@
+'use client';
+import React, { useEffect } from 'react';
+import { Text } from '@/components/common/Text';
+import { IconClose } from '@/public/icons';
+
+interface BottomSheetProps {
+	isVisible: boolean;
+	onClose: () => void;
+	children: React.ReactNode;
+}
+
+export const BottomSheet: React.FC<BottomSheetProps> = ({
+	isVisible,
+	onClose,
+	children,
+}) => {
+	const bottomSheetClasses = isVisible ? 'translate-y-0' : 'translate-y-full';
+	const overlayClasses = isVisible
+		? 'opacity-50 pointer-events-auto'
+		: 'opacity-0 pointer-events-none';
+
+	useEffect(() => {
+		const scrollContentsElement = document.querySelector('.scroll-contents');
+
+		if (isVisible) {
+			document.body.style.overflow = 'hidden';
+			if (scrollContentsElement) {
+				scrollContentsElement.classList.remove('scroll-contents');
+			}
+		} else {
+			document.body.style.overflow = '';
+			if (scrollContentsElement) {
+				scrollContentsElement.classList.add('scroll-contents');
+			}
+		}
+
+		return () => {
+			document.body.style.overflow = '';
+			if (scrollContentsElement) {
+				scrollContentsElement.classList.add('scroll-contents');
+			}
+		};
+	}, [isVisible]);
+
+	return (
+		<>
+			<div
+				className={`fixed inset-0 z-[998] bg-black transition-opacity duration-300 ease-in-out ${overlayClasses}`}
+				onClick={onClose}
+			/>
+			<div
+				onClick={(e) => e.stopPropagation()}
+				className={`fixed bottom-0 left-0 z-[999] w-full bg-white shadow-lg transition-transform duration-300 ease-in-out transform ${bottomSheetClasses} py-20pxr rounded-t-12pxr`}
+			>
+				<div className="flex-1 overflow-y-auto px-20pxr">{children}</div>
+			</div>
+		</>
+	);
+};
+
+export default BottomSheet;

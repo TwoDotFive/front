af7f73e76f621ae1b804bae57b95fa2581107bb8
wonho1401 <wonho1401@naver.com>
2024-09-03 23:50:01 +0900

    fix: 팬풀로그 만들기일 때 NavBar 안보이게 수정

--- src/components/common/navbar/NavBar.tsx
+++ src/components/common/navbar/NavBar.tsx
@@ -1,8 +1,13 @@
 'use client';
 import { navBarList } from '@/constants/navBarList';
 import NavItem from './NavBarItem';
+import { usePathname } from 'next/navigation';
 
 export default function NavBar() {
+	const pathname = usePathname();
+
+	if(/^\/fanpool-log\/create-log\/step[1-3]/.test(pathname)) return null;
+
 	return (
 		<div className="z-50 px-[34.5px] py-[8.5px] absolute bottom-0 flex w-full items-center justify-between bg-white rounded-t-12pxr">
 			{navBarList.map((navItem) => (

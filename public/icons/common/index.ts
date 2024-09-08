30fc203083c1e443eed5d8e9c22cabf8d0be3fac
wonho1401 <wonho1401@naver.com>
2024-09-09 01:17:52 +0900

    chore: 회색 햄버거 아이콘 추가

--- public/icons/common/index.ts
+++ public/icons/common/index.ts
@@ -1,43 +1,45 @@
-import IconCalender from './icon_calender.svg';
-import IconCancel from './icon_cancel.svg';
-import IconLeftArrow from './icon_left_arrow.svg';
-import IconRightArrow from './icon_right_arrow.svg';
-import IconMoreMenu from './icon_more_menu.svg';
-import IconPencil from './icon_pencil.svg';
-import IconPlus from './icon_plus.svg';
-import IconUpload from './icon_upload.svg';
-import IconCheck from './icon_check.svg';
-import IconRadioDefault from './icon_radio_default.svg';
-import IconRadioSelected from './icon_radio_selected.svg';
-import IconButtonPlus from './icon_button_plus.svg';
-import IconButtonMinus from './icon_button_minus.svg';
-import IconHamburger from './icon_hamburger.svg';
-import IconPerson from './icon_person.svg';
-import IconClose from './icon_close.svg';
-import IconShare from './icon_share.svg';
-import IconLink from './icon_link.svg';
-import IconBookMark from './icon_bookmark.svg';
-import IconBookMarkSelected from './icon_bookmark_selected.svg';
+import IconCalender from "./icon_calender.svg";
+import IconCancel from "./icon_cancel.svg";
+import IconLeftArrow from "./icon_left_arrow.svg";
+import IconRightArrow from "./icon_right_arrow.svg";
+import IconMoreMenu from "./icon_more_menu.svg";
+import IconPencil from "./icon_pencil.svg";
+import IconPlus from "./icon_plus.svg";
+import IconUpload from "./icon_upload.svg";
+import IconCheck from "./icon_check.svg";
+import IconRadioDefault from "./icon_radio_default.svg";
+import IconRadioSelected from "./icon_radio_selected.svg";
+import IconButtonPlus from "./icon_button_plus.svg";
+import IconButtonMinus from "./icon_button_minus.svg";
+import IconHamburger from "./icon_hamburger.svg";
+import IconHamburgerGray from "./icon_hamburger_gray.svg";
+import IconPerson from "./icon_person.svg";
+import IconClose from "./icon_close.svg";
+import IconShare from "./icon_share.svg";
+import IconLink from "./icon_link.svg";
+import IconBookMark from "./icon_bookmark.svg";
+import IconBookMarkSelected from "./icon_bookmark_selected.svg";
 
 export {
-	IconCalender,
-	IconCancel,
-	IconLeftArrow,
-	IconRightArrow,
-	IconMoreMenu,
-	IconPencil,
-	IconPlus,
-	IconUpload,
-	IconCheck,
-	IconRadioDefault,
-	IconRadioSelected,
-	IconButtonPlus,
-	IconButtonMinus,
-	IconHamburger,
-	IconPerson,
-	IconClose,
-	IconShare,
-	IconLink,
-	IconBookMark,
-	IconBookMarkSelected
+  IconCalender,
+  IconCancel,
+  IconLeftArrow,
+  IconRightArrow,
+  IconMoreMenu,
+  IconPencil,
+  IconPlus,
+  IconUpload,
+  IconCheck,
+  IconRadioDefault,
+  IconRadioSelected,
+  IconButtonPlus,
+  IconButtonMinus,
+  IconHamburger,
+  IconHamburgerGray,
+  IconPerson,
+  IconClose,
+  IconShare,
+  IconLink,
+  IconBookMark,
+  IconBookMarkSelected,
 };

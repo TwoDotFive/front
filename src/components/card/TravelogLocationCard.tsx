5abbebea9e92ce3e68a3229b21e9e33594623c58
wonho1401 <wonho1401@naver.com>
2024-09-09 01:17:35 +0900

    edit: 여행기 장소 카드 props 추가

--- src/components/card/TravelogLocationCard.tsx
+++ src/components/card/TravelogLocationCard.tsx
@@ -1,40 +1,51 @@
-import React from 'react';
-import LocationInfoMiniCard from './LocationInfoMiniCard';
-import Button from '../common/Button';
+import React from "react";
+import LocationInfoMiniCard from "./LocationInfoMiniCard";
+import Button from "../common/Button";
 
 type TravelogLocationCardProps = {
-    image: string;
-    name: string;
-    location: string;
-    userId :string;  // 수정 필요
-}
+  image: string;
+  name: string;
+  location: string;
+  onClick: () => void;
+  onRemove?: () => void;
+  isEditing: boolean;
+};
 
-export default function TravelogLocationCard({image, name, location, userId}: TravelogLocationCardProps){
-    if(userId !== 'myUserId'){
-        return (
-            <div className='flex w-300pxr p-12pxr items-center gap-8pxr flex-shrink-0 rounded-8pxr bg-gray000 shadow-[2px_2px_20px_0px_rgba(47,47,48,0.10)]'>
-                <LocationInfoMiniCard image={image} name={name} location={location} />
-            </div>
-        );
-    }
-    return (
-        <div className='flex w-300pxr p-12pxr items-center gap-8pxr flex-shrink-0 rounded-8pxr bg-gray000 shadow-[2px_2px_20px_0px_rgba(47,47,48,0.10)]'>
-            <div className='flex flex-col items-start gap-8pxr'>
-                <LocationInfoMiniCard image={image} name={name} location={location} />
-                <Button
-					width="276px"
-					height="40px"
-					text={'메모추가'}
-					borderRadius={8}
-					fontSize={14}
-					fontWeight={700}
-					enabledTextColor={'text-kboBlue500'}
-					enabledBackgroundColor={'bg-kboBlue0'}
-					disabledTextColor={'text-gray600'}
-					disabledBackgroundColor={'bg-gray050'}
-					onClick={() => {}}
-				/>
-            </div>
-        </div>
-    )
+export default function TravelogLocationCard({
+  image,
+  name,
+  location,
+  onClick,
+  onRemove,
+  isEditing,
+}: TravelogLocationCardProps) {
+  return (
+    <div className="flex w-300pxr p-12pxr items-center gap-8pxr flex-shrink-0 rounded-8pxr bg-gray000 shadow-[2px_2px_20px_0px_rgba(47,47,48,0.10)]">
+      <div className="flex flex-col items-start gap-8pxr">
+        <LocationInfoMiniCard
+          image={image}
+          name={name}
+          location={location}
+          isEditing={isEditing}
+          onRemove={onRemove} // X 버튼 클릭 시 동작할 삭제 함수
+        />
+        {/* 순서 바꾸기 모드가 아닐 때만 메모추가 버튼 표시 */}
+        {!isEditing && (
+          <Button
+            width="276px"
+            height="40px"
+            text={"메모추가"}
+            borderRadius={8}
+            fontSize={14}
+            fontWeight={700}
+            enabledTextColor={"text-kboBlue500"}
+            enabledBackgroundColor={"bg-kboBlue0"}
+            disabledTextColor={"text-gray600"}
+            disabledBackgroundColor={"bg-gray050"}
+            onClick={onClick}
+          />
+        )}
+      </div>
+    </div>
+  );
 }

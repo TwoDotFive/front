25f7ce627a6a16c3172b98a05dcef25258ccc7e6
wonho1401 <wonho1401@naver.com>
2024-09-14 02:21:38 +0900

    edit: api에 맞게 type 수정

--- src/components/common/button/SelectAreaButton.tsx
+++ src/components/common/button/SelectAreaButton.tsx
@@ -1,37 +1,34 @@
-import React from 'react';
-import { regions } from '@/constants/regions';
-import { Text } from '../Text';
+import React from "react";
+import { Text } from "../Text";
 
 type SelectAreaButtonProps = {
-	code: string;
-	isSelected?: boolean;
-	onClick?: () => void;
+  area: string;
+  isSelected?: boolean;
+  onClick?: () => void;
 };
 
 export default function SelectAreaButton({
-	code,
-	isSelected = false,
-	onClick,
+  area,
+  isSelected = false,
+  onClick,
 }: SelectAreaButtonProps) {
-	const region = regions.find((region) => region.code === code);
-
-	return (
-		<div
-			className={`w-full h-60pxr flex flex-col items-center justify-center rounded-8pxr px-16pxr py-11pxr gap-2pxr cursor-pointer ${
-				isSelected
-					? 'bg-activeLylac100 border border-activeLylac300'
-					: 'bg-gray050'
-			}`}
-			onClick={onClick}
-		>
-			<Text
-				fontSize={16}
-				fontWeight={700}
-				color={isSelected ? 'activeLylac600' : 'gray700'}
-				className="whitespace-nowrap"
-			>
-				{region!.area}
-			</Text>
-		</div>
-	);
+  return (
+    <div
+      className={`w-full h-60pxr flex flex-col items-center justify-center rounded-8pxr px-16pxr py-11pxr gap-2pxr cursor-pointer ${
+        isSelected
+          ? "bg-activeLylac100 border border-activeLylac300"
+          : "bg-gray050"
+      }`}
+      onClick={onClick}
+    >
+      <Text
+        fontSize={16}
+        fontWeight={700}
+        color={isSelected ? "activeLylac600" : "gray700"}
+        className="whitespace-nowrap"
+      >
+        {area}
+      </Text>
+    </div>
+  );
 }

fd138ff9e260df493bba51f1b515524482b6b06e
wonho1401 <wonho1401@naver.com>
2024-09-21 20:22:06 +0900

    edit: contentId, contentType props 추가

--- src/components/card/LocationInfoSearchCard.tsx
+++ src/components/card/LocationInfoSearchCard.tsx
@@ -1,34 +1,54 @@
-import React from 'react';
-import { Text } from '../common/Text';
-import SelectButton from '../common/button/SelectButton';
+import React from "react";
+import { Text } from "../common/Text";
+import SelectButton from "../common/button/SelectButton";
+import { useRouter } from "next/navigation";
 
 type LocationInfoSearchCardProps = {
-    image: string;
-    name: string;
-    location: string;
-    isSelected: boolean;
-    onClick: () => void;
-}
+  image: string;
+  name: string;
+  location: string;
+  contentId: number;
+  contentType: string;
+  isSelected: boolean;
+  onClick: () => void;
+};
 
-export default function LocationInfoSearchCard({image, name, location, isSelected, onClick}: LocationInfoSearchCardProps){
-    return (
-        <div className='flex items-center gap-4pxr w-full h-50pxr'>
-            <div className='flex items-center gap-8pxr flex-1'>
-                <div className='w-50pxr h-50pxr rounded-5pxr'>
-                    <img className='w-full h-full rounded-5pxr' src={image}/>
-                </div>
-                <div className='flex flex-col justify-between items-start flex-1 self-stretch'>
-                    <Text fontSize={16} fontWeight={700} color='gray700'>
-                        {name}
-                    </Text>
-                    <Text fontSize={14} fontWeight={400} color='gray600'>
-                        {location}
-                    </Text>
-                </div>
-            </div>
-            <div className='ml-auto'>
-                <SelectButton isSelected={isSelected} onClick={onClick}/>
-            </div>
+export default function LocationInfoSearchCard({
+  image,
+  name,
+  location,
+  contentId,
+  contentType,
+  isSelected,
+  onClick,
+}: LocationInfoSearchCardProps) {
+  const router = useRouter();
+
+  const handleCardClick = () => {
+    router.push(`/fanpool-log/place/${contentId}/?contentType=${contentType}`);
+  };
+
+  return (
+    <div className="flex items-center gap-4pxr w-full h-50pxr">
+      <div className="flex items-center gap-8pxr flex-1">
+        <div className="w-50pxr h-50pxr rounded-5pxr">
+          <img className="w-full h-full rounded-5pxr" src={image} />
         </div>
-    );
+        <div
+          className="flex flex-col justify-between items-start flex-1 self-stretch"
+          onClick={handleCardClick}
+        >
+          <Text fontSize={16} fontWeight={700} color="gray700">
+            {name}
+          </Text>
+          <Text fontSize={14} fontWeight={400} color="gray600">
+            {location}
+          </Text>
+        </div>
+      </div>
+      <div className="ml-auto">
+        <SelectButton isSelected={isSelected} onClick={onClick} />
+      </div>
+    </div>
+  );
 }

9b1fc2f81cca10fd31df94a8e7b9bb94c8d9a1a0
wonho1401 <wonho1401@naver.com>
2024-09-21 22:08:30 +0900

    feat: 국문장소상세보기 타입 추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,42 +1,58 @@
 export interface Team {
-	id: number;
-	name: string;
-	representativeImageUrl: string;
-	stadiumName: string;
-	stadiumAliasName: string;
+  id: number;
+  name: string;
+  representativeImageUrl: string;
+  stadiumName: string;
+  stadiumAliasName: string;
 }
 
 export interface Game {
-	id: number;
-	awayTeam: Team;
-	homeTeam: Team;
-	startDate: string;
-	stadium: string;
+  id: number;
+  awayTeam: Team;
+  homeTeam: Team;
+  startDate: string;
+  stadium: string;
 }
 
 export interface Location {
-	fullText: string;
-	zipNo: string;
-	sido: string;
-	sigungu: string;
-	dong: string;
-	dongCd: string;
-	x: string;
-	y: string;
+  fullText: string;
+  zipNo: string;
+  sido: string;
+  sigungu: string;
+  dong: string;
+  dongCd: string;
+  x: string;
+  y: string;
 }
 
 export interface FanpoolInformation {
-	id: number;
-	hostUserId: number;
-	game: Game;
-	departAt: string;
-	departFrom: Location;
-	fanpoolType: string;
-	fanpoolTypeKor: string;
-	numberOfPeople: number;
-	currentNumberOfPeople: number;
+  id: number;
+  hostUserId: number;
+  game: Game;
+  departAt: string;
+  departFrom: Location;
+  fanpoolType: string;
+  fanpoolTypeKor: string;
+  numberOfPeople: number;
+  currentNumberOfPeople: number;
 }
 
 export interface FanpoolType {
-	fanpoolInformation: FanpoolInformation;
+  fanpoolInformation: FanpoolInformation;
+}
+
+export interface PlaceContent {
+  checkInTime: string | null;
+  checkOutTime: string | null;
+  contentId: number;
+  contentType: number;
+  fee: number | null;
+  firstMenu: string | null;
+  infoCenter: string | null;
+  openTime: string | null;
+  parking: string | null;
+  restDate: string | null;
+  reservervationPageUrl: string | null;
+  restroom: string | null;
+  treatMenu: string | null;
 }

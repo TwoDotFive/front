289bc1fd7cfed865a3eaff108efa415344a27df9
wonho1401 <wonho1401@naver.com>
2024-09-23 00:10:07 +0900

    feat: 팬풀로그 상세보기 페이지 api 연동

--- src/app/(tarbar)/fanpool-log/log/[id]/page.tsx
+++ src/app/(tarbar)/fanpool-log/log/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { deleteFanpoolLog, getFanpoolLog } from "@/api/fanpool-log/log/main";
+import TravelogAddCard from "@/components/card/TravelogAddCard";
 import TravelogLocationCard from "@/components/card/TravelogLocationCard";
 import Button from "@/components/common/Button";
 import InfinityLine from "@/components/common/InfinityLine";
@@ -14,71 +16,67 @@ import {
   IconLink,
   IconShare,
 } from "@/public/icons";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Schedule } from "@/store/fanpool-log/store";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+
+interface DetailPageProps {
+  id: number;
+  title: string;
+  image: string | null;
+  stadium: string;
+  user: {
+    nickname: string;
+    image: string;
+  };
+  schedules: Schedule[];
+}
 
 export default function FanpoolLogDetailPage() {
   useKakaoLoader();
 
   const [rImage, setRImage] = useState<string | null>(null);
 
+  const [fanpoolLog, setFanpoolLog] = useState<DetailPageProps | null>(null);
+
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+  const params = useParams();
+  const { id } = params;
 
   const [isSelected, setIsSelected] = useState<boolean>(false);
   const handleBookMarkButton = () => {
     setIsSelected(!isSelected);
   };
 
-  const locations = [
-    {
-      id: 1,
-      name: "잠실종합운동장 잠실야구장",
-      location: "서울 송파구 올림픽로 25",
-      image: "/images/doosan.png",
-    },
-    {
-      id: 2,
-      name: "장소 2",
-      location: "서울 송파구 올림픽로 25",
-      image: "/images/kt.png",
-    },
-    {
-      id: 3,
-      name: "장소 3",
-      location: "서울 송파구 올림픽로 25",
-      image: "/images/samsung.png",
-    },
-    {
-      id: 4,
-      name: "장소 4",
-      location: "서울 송파구 올림픽로 25",
-      image: "/images/ssg.png",
-    },
-  ];
+  useEffect(() => {
+    getFanpoolLog(id.toString()).then((res) => {
+      setFanpoolLog(res.data);
+    });
+  }, []);
+
+  if (!fanpoolLog) return <div>Loading...</div>;
 
   return (
     <div className="w-full">
       <TapBar text="" type="left" />
       {/* 팬풀 로그 타이틀 및 장소 */}
       <div className="w-full flex flex-col items-center gap-4pxr px-20pxr">
-        {rImage && (
+        {fanpoolLog.image && (
           <div className="w-85pxr h-85pxr">
             <img
               className="w-full h-full object-cover rounded-lg"
-              src={rImage}
+              src={fanpoolLog.image}
               alt={"Representative Image"}
             />
           </div>
         )}
 
         <Text fontSize={20} fontWeight={700} color="gray700">
-          비오는날 경기 대신 서울나들이
+          {fanpoolLog.title}
         </Text>
         <Text fontSize={16} fontWeight={400} color="gray600">
-          잠실 야구장
+          {fanpoolLog.stadium}
         </Text>
       </div>
       <div className="mt-18pxr" />
@@ -95,10 +93,27 @@ export default function FanpoolLogDetailPage() {
           width: "100%",
           height: "170px",
         }}
-        level={3}
-      />
+        level={7}
+      >
+        {fanpoolLog.schedules.map((schedule, index) => (
+          <MapMarker
+            key={index}
+            position={{
+              lat: schedule.place.y,
+              lng: schedule.place.x,
+            }}
+            image={{
+              src: "icons/map/icons_default_pin.svg",
+              size: { width: 28, height: 40 },
+            }}
+          />
+        ))}
+      </Map>
       {/* 팬풀 등록자 */}
-      <FanpoologUser />
+      <FanpoologUser
+        name={fanpoolLog.user.nickname}
+        image={fanpoolLog.user.image}
+      />
 
       <InfinityLine
         color="bg-gray-50"
@@ -109,39 +124,64 @@ export default function FanpoolLogDetailPage() {
 
       {/* 각 장소 카드*/}
       <div className="relative flex flex-col items-start px-20pxr">
-        <Text fontSize={18} fontWeight={700} color="gray800">
-          Day 1
-        </Text>
-        <div className="mt-18pxr" />
-        {locations.map((location, index) => (
-          <div
-            key={location.id}
-            className="relative flex items-center mb-8 w-full"
-          >
-            {/* 마커 번호 */}
-            <div className="relative z-10">
-              <div className="flex items-center justify-center">
-                <IconDefaultPin />
-              </div>
-              <span className="absolute inset-0 flex items-center justify-center">
-                <Text fontSize={12} fontWeight={700} color="white">
-                  {index + 1}
-                </Text>
-              </span>
-            </div>
-            {/* 장소 카드 */}
-            <div className="ml-16pxr w-full">
-              <TravelogLocationCard
-                image={location.image}
-                name={location.name}
-                location={location.location}
-                onClick={() => {}}
-                isEditing={false}
-              />
-            </div>
+        {Array.from(
+          new Set(fanpoolLog.schedules.map((schedule) => schedule.day))
+        ).map((day, dayIndex) => (
+          <div key={dayIndex} className="w-full">
+            <Text
+              fontSize={18}
+              fontWeight={700}
+              color="gray800"
+            >{`Day ${day}`}</Text>
+            <div className="mt-18pxr" />
+
+            {fanpoolLog.schedules
+              .filter((schedule) => schedule.day === day)
+              .map((schedule, index) => (
+                <div
+                  key={schedule.place.contentId}
+                  className="relative flex items-center mb-16pxr w-full"
+                >
+                  <div className="relative z-10">
+                    <div className="flex items-center justify-center">
+                      <IconDefaultPin />
+                    </div>
+                    <span className="absolute inset-0 flex items-center justify-center">
+                      <Text fontSize={12} fontWeight={700} color="white">
+                        {index + 1}
+                      </Text>
+                    </span>
+                  </div>
+                  <div className="ml-16pxr w-full">
+                    {schedule.memo && schedule.memo.content ? (
+                      <TravelogAddCard
+                        image={schedule.place.thumbnail}
+                        name={schedule.place.name}
+                        location={schedule.place.address}
+                        description={schedule.memo.content}
+                        userId={"myUserId"}
+                        locationImage={schedule.memo.images?.map(
+                          (img) => img.url
+                        )}
+                        onClick={() => {}}
+                        isEditing={true}
+                      />
+                    ) : (
+                      <TravelogLocationCard
+                        image={schedule.place.thumbnail}
+                        name={schedule.place.name}
+                        location={schedule.place.address}
+                        isEditing={true}
+                        onClick={() => {}}
+                      />
+                    )}
+                  </div>
+                </div>
+              ))}
           </div>
         ))}
       </div>
+      <div className="mb-50pxr" />
       {/* 바텀 시트 */}
       <div
         className={

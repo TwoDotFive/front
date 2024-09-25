acae4f5d5e2471a32819c0c9c8834e183051efd4
wonho1401 <wonho1401@naver.com>
2024-09-26 00:02:27 +0900

    edit: 편집을 위한 타입 추가

--- src/components/common/TapBar.tsx
+++ src/components/common/TapBar.tsx
@@ -1,86 +1,98 @@
-'use client';
-import { IconLeftArrow, IconRightArrow, IconUpload } from '@/public/icons';
-import { useRouter } from 'next/navigation';
-import { Text } from './Text';
+"use client";
+import { IconLeftArrow, IconRightArrow, IconUpload } from "@/public/icons";
+import { useRouter } from "next/navigation";
+import { Text } from "./Text";
 
 type TapBarProps = {
-	text: string;
-	isNextButton?: boolean;
-	type: 'download' | 'left' | 'mid' | 'none';
+  text: string;
+  isNextButton?: boolean;
+  type: "download" | "left" | "mid" | "edit" | "none";
+  onClick?: () => void;
 };
 
 export default function TapBar({
-	text,
-	isNextButton = false,
-	type,
+  text,
+  isNextButton = false,
+  type,
+  onClick,
 }: TapBarProps) {
-	const router = useRouter();
+  const router = useRouter();
 
-	const handleBack = () => {
-		router.back();
-	};
+  const handleBack = () => {
+    router.back();
+  };
 
-	const handleNext = () => {
-		router.forward();
-	};
+  const handleNext = () => {
+    router.forward();
+  };
 
-	const renderLeftSection = () => {
-		if (type === 'mid') {
-			return (
-				<>
-					<IconLeftArrow className="cursor-pointer" onClick={handleBack} />
-					<Text fontSize={18} fontWeight={500} color="gray700">
-						{text}
-					</Text>
-				</>
-			);
-		}
+  const renderLeftSection = () => {
+    if (type === "mid") {
+      return (
+        <>
+          <IconLeftArrow className="cursor-pointer" onClick={handleBack} />
+          <Text fontSize={18} fontWeight={500} color="gray700">
+            {text}
+          </Text>
+        </>
+      );
+    }
 
-		return (
-			<div className="flex items-center">
-				<IconLeftArrow className="cursor-pointer" onClick={handleBack} />
-				<Text
-					fontSize={type === 'download' ? 16 : 18}
-					fontWeight={type === 'download' ? 700 : 500}
-					color="gray700"
-					className="ml-4pxr"
-				>
-					{text}
-				</Text>
-			</div>
-		);
-	};
+    return (
+      <div className="flex items-center">
+        <IconLeftArrow className="cursor-pointer" onClick={handleBack} />
+        <Text
+          fontSize={type === "download" ? 16 : 18}
+          fontWeight={type === "download" ? 700 : 500}
+          color="gray700"
+          className="ml-4pxr"
+        >
+          {text}
+        </Text>
+      </div>
+    );
+  };
 
-	const renderRightSection = () => {
-		if (type === 'mid') {
-			if (isNextButton)
-				return (
-					<IconRightArrow className="cursor-pointer" onClick={handleNext} />
-				);
-			else return <div className="w-25pxr" />;
-		}
-		if (type === 'download') {
-			return <IconUpload className="cursor-pointer" />;
-		}
-		return null;
-	};
+  const renderRightSection = () => {
+    if (type === "mid") {
+      if (isNextButton)
+        return (
+          <IconRightArrow className="cursor-pointer" onClick={handleNext} />
+        );
+      else return <div className="w-25pxr" />;
+    }
+    if (type === "download") {
+      return <IconUpload className="cursor-pointer" />;
+    }
+    if (type === "edit") {
+      // 편집 버튼 생성 (누르면 팬풀로그 편집으로 이동)
+      return (
+        <div className="flex items-center" onClick={onClick}>
+          <Text fontSize={16} fontWeight={700} color="gray700">
+            편집
+          </Text>
+        </div>
+      );
+    }
+    return null;
+  };
 
-	if (type === 'none') {
-		return (
-			<Text fontSize={18} fontWeight={700} color="gray700" className="ml-12pxr">
-				{text}
-			</Text>
-		);
-	}
+  if (type === "none") {
+    return (
+      <Text fontSize={18} fontWeight={700} color="gray700" className="ml-12pxr">
+        {text}
+      </Text>
+    );
+  }
 
-	return (
-		<div
-			className={`w-full h-49pxr flex items-center px-12pxr ${
-				type !== 'left' ? 'justify-between' : ''
-			}`}
-		>
-			{renderLeftSection()}
-			{renderRightSection()}
-		</div>
-	);
+  return (
+    <div
+      className={`w-full h-49pxr flex items-center px-12pxr ${
+        type !== "left" ? "justify-between" : ""
+      }`}
+    >
+      {renderLeftSection()}
+      {renderRightSection()}
+    </div>
+  );
 }

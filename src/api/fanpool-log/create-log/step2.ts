392c4db64e34f9cc015153f9721d6166dc08f949
wonho1401 <wonho1401@naver.com>
2024-09-26 01:09:53 +0900

    edit: 무한스크롤을 위한 pageNumbe 파라미터 추가

--- src/api/fanpool-log/create-log/step2.ts
+++ src/api/fanpool-log/create-log/step2.ts
@@ -3,11 +3,11 @@ import apiClient from "@/api/index";
 export const getTourInfo = async (
   x: string,
   y: string,
-  selectedTagId: string | null
+  selectedTagId: string | null,
+  pageNumber: number
 ) => {
   const radius = "1000";
   const pageSize = 10;
-  const pageNumber = 1;
 
   const response = await apiClient.get(
     `${process.env.NEXT_PUBLIC_API_URL}/tour/info`,

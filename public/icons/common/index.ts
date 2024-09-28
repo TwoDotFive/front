5a2e72bd64884d81c253d2316cd9a2fb9727b786
westofsky <westofsky159@gmail.com>
2024-09-28 15:43:56 +0900

    chore: Icon,Image추가

--- public/icons/common/index.ts
+++ public/icons/common/index.ts
@@ -1,77 +1,79 @@
-import IconCalender from "./icon_calender.svg";
-import IconCancel from "./icon_cancel.svg";
-import IconLeftArrow from "./icon_left_arrow.svg";
-import IconRightArrow from "./icon_right_arrow.svg";
-import IconMoreMenu from "./icon_more_menu.svg";
-import IconPencil from "./icon_pencil.svg";
-import IconPencilGray from "./icon_pencil_gray.svg";
-import IconPlus from "./icon_plus.svg";
-import IconUpload from "./icon_upload.svg";
-import IconCheck from "./icon_check.svg";
-import IconRadioDefault from "./icon_radio_default.svg";
-import IconRadioSelected from "./icon_radio_selected.svg";
-import IconButtonPlus from "./icon_button_plus.svg";
-import IconButtonMinus from "./icon_button_minus.svg";
-import IconHamburger from "./icon_hamburger.svg";
-import IconHamburgerGray from "./icon_hamburger_gray.svg";
-import IconPerson from "./icon_person.svg";
-import IconClose from "./icon_close.svg";
-import IconShiftLeft from "./icon_shift_left.svg";
-import IconShiftRight from "./icon_shift_right.svg";
-import IconShiftBottom from "./icon_shift_bottom.svg";
-import IconFavorite from "./icon_favorite.svg";
-import IconFilter from "./icon_filter.svg";
-import IconCheckNavy from "./icon_check_navy.svg";
-import IconChat from "./icon_chat.svg";
-import IconSplash from "./icon_splash.svg";
-import IconSplashEmpty from "./icon_splash_empty.svg";
-import IconKakao from "./icon_kakao.svg";
-import IconLeftArrowWhite from "./icon_left_arrow_white.svg";
-import IconBookmarkUnChecked from "./icon_bookmark_unchecked.svg";
-import IconBookmarkChecked from "./icon_bookmark_checked.svg";
-import IconCar from "./icon_car.svg";
-import IconShare from "./icon_share.svg";
-import IconLink from "./icon_link.svg";
-import IconBookMark from "./icon_bookmark.svg";
-import IconBookMarkSelected from "./icon_bookmark_selected.svg";
-import IconTrashCan from "./icon_trash_can.svg";
+import IconCalender from './icon_calender.svg';
+import IconCancel from './icon_cancel.svg';
+import IconLeftArrow from './icon_left_arrow.svg';
+import IconRightArrow from './icon_right_arrow.svg';
+import IconMoreMenu from './icon_more_menu.svg';
+import IconPencil from './icon_pencil.svg';
+import IconPencilGray from './icon_pencil_gray.svg';
+import IconPlus from './icon_plus.svg';
+import IconUpload from './icon_upload.svg';
+import IconCheck from './icon_check.svg';
+import IconRadioDefault from './icon_radio_default.svg';
+import IconRadioSelected from './icon_radio_selected.svg';
+import IconButtonPlus from './icon_button_plus.svg';
+import IconButtonMinus from './icon_button_minus.svg';
+import IconHamburger from './icon_hamburger.svg';
+import IconHamburgerGray from './icon_hamburger_gray.svg';
+import IconPerson from './icon_person.svg';
+import IconClose from './icon_close.svg';
+import IconShiftLeft from './icon_shift_left.svg';
+import IconShiftRight from './icon_shift_right.svg';
+import IconShiftBottom from './icon_shift_bottom.svg';
+import IconFavorite from './icon_favorite.svg';
+import IconFilter from './icon_filter.svg';
+import IconCheckNavy from './icon_check_navy.svg';
+import IconChat from './icon_chat.svg';
+import IconSplash from './icon_splash.svg';
+import IconSplashEmpty from './icon_splash_empty.svg';
+import IconKakao from './icon_kakao.svg';
+import IconLeftArrowWhite from './icon_left_arrow_white.svg';
+import IconBookmarkUnChecked from './icon_bookmark_unchecked.svg';
+import IconBookmarkChecked from './icon_bookmark_checked.svg';
+import IconCar from './icon_car.svg';
+import IconShare from './icon_share.svg';
+import IconLink from './icon_link.svg';
+import IconBookMark from './icon_bookmark.svg';
+import IconBookMarkSelected from './icon_bookmark_selected.svg';
+import IconTrashCan from './icon_trash_can.svg';
+import IconUnChecked from './IconUnChecked.svg';
 
 export {
-  IconCalender,
-  IconCancel,
-  IconLeftArrow,
-  IconRightArrow,
-  IconMoreMenu,
-  IconPencil,
+	IconCalender,
+	IconCancel,
+	IconLeftArrow,
+	IconRightArrow,
+	IconMoreMenu,
+	IconPencil,
 	IconPencilGray,
-  IconPlus,
-  IconUpload,
-  IconCheck,
-  IconRadioDefault,
-  IconRadioSelected,
-  IconButtonPlus,
-  IconButtonMinus,
-  IconHamburger,
-  IconHamburgerGray,
-  IconPerson,
-  IconClose,
-  IconShiftLeft,
-  IconShiftRight,
-  IconShiftBottom,
-  IconFavorite,
-  IconFilter,
-  IconCheckNavy,
-  IconChat,
- 	IconSplash,
+	IconPlus,
+	IconUpload,
+	IconCheck,
+	IconRadioDefault,
+	IconRadioSelected,
+	IconButtonPlus,
+	IconButtonMinus,
+	IconHamburger,
+	IconHamburgerGray,
+	IconPerson,
+	IconClose,
+	IconShiftLeft,
+	IconShiftRight,
+	IconShiftBottom,
+	IconFavorite,
+	IconFilter,
+	IconCheckNavy,
+	IconChat,
+	IconSplash,
 	IconSplashEmpty,
 	IconKakao,
 	IconLeftArrowWhite,
 	IconBookmarkUnChecked,
 	IconBookmarkChecked,
 	IconCar,
-  IconShare,
-  IconLink,
-  IconBookMark,
-  IconBookMarkSelected,
-  IconTrashCan,
+	IconShare,
+	IconLink,
+	IconBookMark,
+	IconBookMarkSelected,
+	IconTrashCan,
+	IconUnChecked,
 };

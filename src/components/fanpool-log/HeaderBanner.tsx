45d2498f102494598981a9003934e87d9f569321
wonho1401 <wonho1401@naver.com>
2024-09-28 13:50:46 +0900

    edit: 사용자 CSS 수정

--- src/components/fanpool-log/HeaderBanner.tsx
+++ src/components/fanpool-log/HeaderBanner.tsx
@@ -1,61 +1,67 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { Text } from "../common/Text";
 
 interface HeaderBannerProps {
-    imageUrl: string;
-    title: string;
-    description: string;
-    nickname: string;
-    userImgUrl: string;
+  imageUrl: string;
+  title: string;
+  description: string;
+  nickname: string;
+  userImgUrl: string;
 }
 
-export default function HeaderBanner({imageUrl, title, description, nickname, userImgUrl}: HeaderBannerProps) {
-    return (
-        <>
-            <section className="relative w-full h-337pxr">
-                <Image
-                    src={imageUrl}
-                    width={399}
-                    height={0}
-                    alt={"kt"}
-                    className="w-full h-full object-cover"
-                />
-                <div
-                    className="absolute flex flex-col gap-[2vw]"
-                    style={{
-                        top: 'calc(50% + 100px)',
-                        padding: '0 20px',
-                        transform: 'translateY(-50%)',
-                    }}
-                >
-                        <Text fontWeight={700} color="white" fontSize={24}>
-                            {title}
-                        </Text>
-                        <Text fontWeight={400} color="white" fontSize={12}>
-                        {description}
-                        </Text>
-                    
-                    <div className="flex gap-4pxr">
-                        <Image
-                            src={userImgUrl}
-                            width={399}
-                            height={0}
-                            alt={"kt"}
-                            className="w-18pxr h-18pxr object-cover rounded-full"
-                        />
-                        <Text
-                            fontWeight={700}
-                            color="white"
-                            fontSize={12}
-                            className="cursor-pointer"
-                        >
-                            {nickname}
-                        </Text>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+export default function HeaderBanner({
+  imageUrl,
+  title,
+  description,
+  nickname,
+  userImgUrl,
+}: HeaderBannerProps) {
+  return (
+    <>
+      <section className="relative w-full h-337pxr">
+        <Image
+          src={imageUrl}
+          width={399}
+          height={0}
+          alt={"kt"}
+          className="w-full h-full object-cover"
+        />
+        <div
+          className="absolute flex flex-col gap-18pxr"
+          style={{
+            top: "calc(50% + 100px)",
+            padding: "0 20px",
+            transform: "translateY(-50%)",
+          }}
+        >
+          <Text fontWeight={700} color="white" fontSize={24}>
+            {title}
+          </Text>
+          <Text fontWeight={400} color="white" fontSize={12}>
+            {description}
+          </Text>
+
+          <div className="flex gap-4pxr">
+            <Image
+              src={userImgUrl}
+              width={399}
+              height={0}
+              alt={"kt"}
+              className="w-18pxr h-18pxr object-cover rounded-full"
+            />
+            <Text
+              fontWeight={700}
+              color="white"
+              fontSize={12}
+              className="cursor-pointer"
+            >
+              {nickname}
+            </Text>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }

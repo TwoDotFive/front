ec94cc748cbb86a226f94d1ab7744d3bae57f7b2
westofsky <westofsky159@gmail.com>
2024-09-30 08:15:56 +0900

    fix: 바텀시트 보여주는 데이터 수정

--- src/components/fanpool-find/PlaceSearchBottomSheet.tsx
+++ src/components/fanpool-find/PlaceSearchBottomSheet.tsx
@@ -7,7 +7,7 @@ import useKakaoLoader from '../register/useKakaoLoader';
 interface PlaceSearchBottomSheetProps {
 	isVisible: boolean;
 	onClose: () => void;
-	onSelectPlace: (place: { name: string; id: string }) => void;
+	onSelectPlace: (place: { name: string; x: string; y: string }) => void;
 }
 
 export const PlaceSearchBottomSheet = ({
@@ -32,7 +32,6 @@ export const PlaceSearchBottomSheet = ({
 		});
 	};
 
-	// query가 변경될 때마다 검색 실행
 	useEffect(() => {
 		if (query.trim()) {
 			const debounce = setTimeout(() => {
@@ -46,7 +45,7 @@ export const PlaceSearchBottomSheet = ({
 	}, [query]);
 
 	const handleSelectPlace = (place: any) => {
-		onSelectPlace({ name: place.place_name, id: place.id });
+		onSelectPlace({ name: place.place_name, x: place.x, y: place.y });
 		onClose();
 	};
 
@@ -72,10 +71,13 @@ export const PlaceSearchBottomSheet = ({
 								<div
 									key={index}
 									onClick={() => handleSelectPlace(place)}
-									className="cursor-pointer py-4pxr underline"
+									className="cursor-pointer py-4pxr"
 								>
-									<Text fontSize={16} fontWeight={500}>
+									<Text fontSize={14} fontWeight={700} color="gray700">
 										{place.place_name}
+									</Text>
+									<Text fontSize={14} fontWeight={400} color="gray700">
+										{place.road_address_name}
 									</Text>
 								</div>
 							))}

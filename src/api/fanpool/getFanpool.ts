2f320a72fad7fd6b3bea6d4f8c005df867673ba0
westofsky <westofsky159@gmail.com>
2024-09-29 10:14:20 +0900

    feat: 프로필 팬풀 탭 수정

--- src/api/fanpool/getFanpool.ts
+++ src/api/fanpool/getFanpool.ts
@@ -11,7 +11,7 @@ interface FanpoolResponse {
 /**
  * 팬풀 정보를 가져오는 함수
  *
- * @param {number} userId 사용자 고유 ID (필수)
+ * @param {string} userId 사용자 고유 ID (필수)
  * @param {number} [page=0] 페이지 넘버 (기본값: 0)
  * @param {number} [size=10] 페이지 사이즈 (기본값: 10)
  *
@@ -22,7 +22,7 @@ const getFanpool = async ({
 	page = 0,
 	size = 10,
 }: {
-	userId: number;
+	userId: string;
 	page?: number;
 	size?: number;
 }): Promise<FanpoolResponse> => {
--- src/components/profile/ProfileTabMenu.tsx
+++ src/components/profile/ProfileTabMenu.tsx
@@ -1,23 +1,73 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Text } from '../common/Text';
 import MakeFanpoolButton from '../common/button/MakeFanpoolButton';
 import MakeFanpoolLogButton from '../common/button/MakeFanpoolLogButton';
 import { useRouter } from 'next/navigation';
 import { useUserStore } from '@/store/useUserStore';
+import TravelogWideCard from '../card/TravelogWideCard';
+import getFanpool from '@/api/fanpool/getFanpool';
+import { getFanpoolLogsByUser } from '@/api/fanpool-log/log/main';
+import { FanpoolInformation } from '@/types/types';
+import FanpoolCard from '../common/fanpool/FanpoolCard';
 
 interface ProfileTabMenuProps {
 	id?: string;
+}
+interface FanpoolLogResponse {
+	items: {
+		id: string;
+		image: string;
+		title: string;
+		stadium: string;
+		profile: {
+			nickname: string;
+			image: string;
+		};
+	}[];
 }
 
 export default function ProfileTabMenu({ id }: ProfileTabMenuProps) {
 	const { userProfile } = useUserStore();
 	const searchId = id || userProfile?.id;
-	const fanpoolCount = 0;
-	console.log(searchId);
-	const logCount = 0;
 	const router = useRouter();
 	const [activeTab, setActiveTab] = useState<'tab1' | 'tab2'>('tab1');
+	const [fanpoolList, setFanpoolList] = useState<FanpoolInformation[]>([]);
+	const [fanpoolLogList, setFanpoolLogList] = useState<
+		FanpoolLogResponse['items']
+	>([]);
+	const [fanpoolCount, setFanpoolCount] = useState(0);
+	const [logCount, setLogCount] = useState(0);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				if (searchId) {
+					// 팬풀 정보 가져오기
+					const fanpoolResponse = await getFanpool({
+						userId: searchId,
+						page: 0,
+						size: 10,
+					});
+					setFanpoolList(fanpoolResponse.fanpoolInformation);
+					setFanpoolCount(fanpoolResponse.fanpoolInformation.length);
+
+					// 팬풀 로그 정보 가져오기
+					const fanpoolLogResponse = await getFanpoolLogsByUser(searchId);
+					setFanpoolLogList(fanpoolLogResponse.items);
+					setLogCount(fanpoolLogResponse.items.length);
+				}
+			} catch (error) {
+				console.error('데이터를 불러오는 중 오류 발생:', error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		// 컴포넌트가 로드될 때 한 번만 실행
+		fetchData();
+	}, [searchId]);
 
 	const handleTabClick = (tab: 'tab1' | 'tab2') => {
 		setActiveTab(tab);
@@ -25,6 +75,67 @@ export default function ProfileTabMenu({ id }: ProfileTabMenuProps) {
 
 	const handleClick = () => {
 		router.push('/fanpool-add');
+	};
+
+	const renderFanpool = () => {
+		return (
+			<div>
+				{fanpoolCount > 0 ? (
+					<div>
+						{fanpoolList.map((fanpool) => (
+							<div key={fanpool.id}>
+								<FanpoolCard fanpool={fanpool} />
+								<div className="h-14pxr" />
+							</div>
+						))}
+					</div>
+				) : (
+					<div className="mt-92pxr flex flex-col gap-14pxr items-center">
+						<Text
+							fontSize={12}
+							fontWeight={500}
+							color="gray500"
+							className="text-center"
+						>
+							모집한 팬풀이 없어요!
+							<br />
+							새로운 팬풀을 모집해봐요!
+						</Text>
+						<MakeFanpoolButton onClick={handleClick} />
+					</div>
+				)}
+			</div>
+		);
+	};
+
+	const renderFanpoolLogList = () => {
+		return fanpoolLogList.length === 0 ? (
+			<div className="mt-92pxr flex flex-col gap-14pxr items-center">
+				<Text
+					fontSize={12}
+					fontWeight={500}
+					color="gray500"
+					className="text-center"
+				>
+					등록된 로그가 없어요
+					<br />
+					지금 새 로그를 만들어 볼까요?
+				</Text>
+				<MakeFanpoolLogButton onClick={() => console.log('Button clicked!')} />
+			</div>
+		) : (
+			<div className="flex flex-col gap-12pxr">
+				{fanpoolLogList.map((fanpoolLog) => (
+					<TravelogWideCard
+						key={fanpoolLog.id}
+						id={fanpoolLog.id}
+						image={fanpoolLog.image}
+						title={fanpoolLog.title}
+						userName={fanpoolLog.profile.nickname}
+					/>
+				))}
+			</div>
+		);
 	};
 
 	return (
@@ -64,54 +175,9 @@ export default function ProfileTabMenu({ id }: ProfileTabMenuProps) {
 			<div className="h-30pxr" />
 			{/* 탭에 따른 컨텐츠 영역 */}
 			<div>
-				{activeTab === 'tab1' && (
-					<div>
-						{fanpoolCount > 0 ? (
-							<Text fontSize={14} fontWeight={500} color="gray600">
-								모집한 팬풀
-							</Text>
-						) : (
-							<div className="mt-92pxr flex flex-col gap-14pxr items-center">
-								<Text
-									fontSize={12}
-									fontWeight={500}
-									color="gray500"
-									className="text-center"
-								>
-									모집한 팬풀이 없어요!
-									<br />
-									새로운 팬풀을 모집해봐요!
-								</Text>
-								<MakeFanpoolButton onClick={handleClick} />
-							</div>
-						)}
-					</div>
-				)}
-				{activeTab === 'tab2' && (
-					<div>
-						{logCount > 0 ? (
-							<Text fontSize={14} fontWeight={500} color="gray600">
-								팬풀로그
-							</Text>
-						) : (
-							<div className="mt-92pxr flex flex-col gap-14pxr items-center">
-								<Text
-									fontSize={12}
-									fontWeight={500}
-									color="gray500"
-									className="text-center"
-								>
-									등록된 로그가 없어요
-									<br />
-									지금 새 로그를 만들어 볼까요?
-								</Text>
-								<MakeFanpoolLogButton
-									onClick={() => console.log('Button clicked!')}
-								/>
-							</div>
-						)}
-					</div>
-				)}
+				{activeTab === 'tab1' && renderFanpool()}
+				{activeTab === 'tab2' && <div>{renderFanpoolLogList()}</div>}
+				<div className="h-100pxr" />
 			</div>
 		</section>
 	);

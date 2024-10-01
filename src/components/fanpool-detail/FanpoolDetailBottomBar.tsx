ae0fdab0ade1f8aaedc430a4f05b077d0d483174
wonho1401 <wonho1401@naver.com>
2024-10-01 21:24:28 +0900

    edit: 팬풀 모집 북마크 버튼 삭제

--- src/components/fanpool-detail/FanpoolDetailBottomBar.tsx
+++ src/components/fanpool-detail/FanpoolDetailBottomBar.tsx
@@ -1,44 +1,42 @@
-import { IconLink, IconShare } from '@/public/icons';
-import Button from '../common/Button';
-import Bookmark from '../common/Bookmark';
-import { FanpoolInformation } from '@/types/types';
+import { IconLink, IconShare } from "@/public/icons";
+import Button from "../common/Button";
+import Bookmark from "../common/Bookmark";
+import { FanpoolInformation } from "@/types/types";
 
 interface FanpoolDetailBottomBarProps {
-	fanpoolInformation: FanpoolInformation;
+  fanpoolInformation: FanpoolInformation;
 }
 
 export default function FanpoolDetailBottomBar({
-	fanpoolInformation,
+  fanpoolInformation,
 }: FanpoolDetailBottomBarProps) {
-	const isHost =
-		fanpoolInformation.hostUserId.toString() == localStorage.getItem('userId');
+  const isHost =
+    fanpoolInformation.hostUserId.toString() == localStorage.getItem("userId");
 
-	return (
-		<section
-			className="max-w-399pxr fixed bottom-0 w-full h-86pxr bg-white px-20pxr pt-16pxr pb-20pxr rounded-16pxr flex gap-8pxr"
-			style={{ boxShadow: '0px 0px 34px 0px rgba(0, 37, 97, 0.10)' }}
-		>
-			<Button
-				height="50px"
-				text={isHost ? '모집 완료하기' : '채팅하기'}
-				borderRadius={8}
-				enabledTextColor={'text-white'}
-				enabledBackgroundColor={'bg-primary'}
-				disabledTextColor={'text-gray300'}
-				disabledBackgroundColor={'bg-gray100'}
-				onClick={() => {}}
-			/>
-			<div className="w-40pxr h-48pxr flex items-center justify-center">
-				<IconShare />
-			</div>
-			<div className="w-40pxr h-48pxr flex items-center justify-center">
-				<IconLink />
-			</div>
-			{!isHost && (
-				<div className="w-40pxr h-48pxr flex items-center justify-center">
-					<Bookmark isChecked={true} onClick={() => {}} />
-				</div>
-			)}
-		</section>
-	);
+  return (
+    <section
+      className="max-w-399pxr fixed bottom-0 w-full h-86pxr bg-white px-20pxr pt-16pxr pb-20pxr rounded-16pxr flex justify-center gap-8pxr"
+      style={{ boxShadow: "0px 0px 34px 0px rgba(0, 37, 97, 0.10)" }}
+    >
+      <Button
+        height="50px"
+        text={isHost ? "모집 완료하기" : "채팅하기"}
+        borderRadius={8}
+        enabledTextColor={"text-white"}
+        enabledBackgroundColor={"bg-primary"}
+        disabledTextColor={"text-gray300"}
+        disabledBackgroundColor={"bg-gray100"}
+        onClick={() => {}}
+      />
+      <button className="flex items-center jusitify-center p-8pxr">
+        <IconShare />
+      </button>
+      <button
+        className="flex items-center jusitify-center p-8pxr"
+        onClick={() => {}}
+      >
+        <IconLink />
+      </button>
+    </section>
+  );
 }

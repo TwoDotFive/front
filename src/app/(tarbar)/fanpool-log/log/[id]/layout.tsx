9eef09353fd4a3aa4cd3b3ef0dd6ce9cb5df6a64
wonho1401 <wonho1401@naver.com>
2024-10-01 22:10:03 +0900

    feat: 팬풀로그 상세보기 카카오톡 공유 구현

--- src/app/(tarbar)/fanpool-log/log/[id]/layout.tsx
+++ src/app/(tarbar)/fanpool-log/log/[id]/layout.tsx
@@ -1,15 +1,20 @@
-import Script from 'next/script'
-import React from 'react'
+import KakaoScript from "@/provider/KakaoScriptProvider";
+import Script from "next/script";
+import React from "react";
 
-export default function layout({children}: {children : React.ReactNode}) {
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services,clusterer&autoload=false`
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services,clusterer&autoload=false`;
   return (
     <>
-        <Script
-            src ={KAKAO_SDK_URL}
-            strategy='beforeInteractive'
-        />
-        {children}
+      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+      <KakaoScript />
+      {children}
     </>
-  )
+  );
 }
